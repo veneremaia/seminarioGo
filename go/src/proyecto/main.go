07dd957117a0a3d7ad578e63607ded1e552106f9
veneremaia <venere.maia@gmail.com>
2019-11-29 15:58:20 -0300

    seminario go

--- go/src/proyecto/main.go
+++ go/src/proyecto/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-	"proyecto/otra"
+	//"proyecto/otra"
+	//"encoding/json"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	//_"github.com/go-sql-drive/mysql" 
+	"errors"
 )
 
 func main(){
+
+	/*
 	fmt.Println("Hello world")
 	fmt.Println()
 
 	c := otra.SumarValores(4,5)
 	fmt.Println(c)
-
+*/
 	autoX := NewAuto("Ford","Nuevo", "a3")
 	autoZ := NewAuto("Chevrolet","Viejo", "b6")
 	autoY := NewAuto("lalala","Masomeno", "b4")
@@ -24,10 +31,100 @@ func main(){
 	
 	fmt.Println(autoA.GetModelo())
 	agenciaX.EliminarAuto(1)
-	autoB := agenciaX.DevolverAuto(1)*/
+	autoB := agenciaX.DevolverAuto(1)
 	autoH := agenciaX.DevolverAutoByPatente("b4")
 	fmt.Println(autoH.GetModelo())
+	
+var b := 5
+	var a := 10
+
+	var c *int  // el tipo de c es un puntero a entero
+	c= &b
+	fmt.Println(&b)   // lugar de memoria de b
+	fmt.Println(c)   //el valor de c es el lugar de memoria de b
+	fmt.Println(&c)  // lugar de memoria de c
+// para imprimir dos cosas, la "," es como el "+"
+*/
+/*
+	myString := "hola como andas"
+	fmt.Println(myString)
+	var a []byte 
+	a, _ = json.Marshal(myString)
+	fmt.Println(a)
+
+
+	var otroString string
+	_=json.Unmarshal(a, &otroString)
+	fmt.Println(otroString)
+	autoL := NewAuto("Ford","Nuevo", "a3")
+
+	autoAMarshalled, _ := json.Marshal(autoL)
+	fmt.Println("AUTO" , autoAMarshalled)
+
+*/
+
+router := gin.Default()
+router.GET("/cars/:patente", agenciaX.GetAutoByPatente)
+
+if err := router.Run(); err !=nil{
+	fmt.Println(err)
 }
+
+
+}
+/*
+func 	AddCar(ctx *gin.Context) {
+    body, err := ioutil.ReadAll(ctx.Request.Body)
+    if err != nil {
+	  fmt.Println("error")
+	  return
+    }
+
+    var car Car
+    if err = json.Unmarshal(body, &car); err != nil {
+		fmt.Println("error")
+		return
+    }
+
+    a.carDatabase.AddCar(car)
+
+    ctx.JSON(http.StatusCreated, gin.H{
+        "message": "car added",
+    })
+}
+*/
+
+
+
+
+func (a *Agencia) GetAutoByPatente(ctx *gin.Context){
+	patente := ctx.Param("patente")
+	
+	 auto, err:= a.DevolverAutoByPatente(patente)
+	
+	 if err !=nil{
+	ctx.JSON(http.StatusNotFound, gin.H{
+		"error": err.Error(),
+	})
+	
+	return
+	
+	}
+		ctx.JSON(http.StatusOK, gin.H{
+	
+			"Auto" : auto,
+	
+	
+		})
+		
+	}
+
+
+
+
+
+
+
 
 type Agencia struct{
 	 autos [] Auto
@@ -61,14 +158,16 @@ func (a *Agencia) setModeloAuto(auto Auto, modelo string){
 	}
 }
 
-func (a *Agencia) DevolverAutoByPatente (patente string) Auto{
-	for i:= 0 ; i<len(a.autos); i++{
-		if a.autos[i].GetPatente() == patente{
-			return a.autos[i]
+
+
+func (a *Agencia) DevolverAutoByPatente (patente string) (*Auto, error){
+	for _, auto:= range a.autos{
+		if auto.patente == patente{
+			return &auto, nil
 		}
 	}
-	return a.autos[0]
-}
+	return nil, errors.New("auto no encontrado")
+}	
 
 
 func (a *Agencia) AgregarAuto( auto Auto){
